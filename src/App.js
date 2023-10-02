b9a18710817d998504ab15f03a5abaafba48212b
Graham Klingler <grahamklingler@gmail.com>
2023-10-01 17:51:15 -0700

    add styles, fonts, parallax on scroll with new links to galleries

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cars from "./Cars.js";
 import Street from "./Street.js";
-import Bio from "./Bio.js";
+import Home from "./Home.js";
+import Bridges from './Bridges.js';
+import Buildings from './Buildings.js';
+import Skylines from './Skylines.js';
+import Bio from './Bio.js';
 
 function App() {
   let reponame = "Portfolio-Project";
@@ -10,10 +14,13 @@ function App() {
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path={`/${reponame}/bio`} element={<Bio />}/>
-          <Route path={`/${reponame}/`} element={<Bio />}/>
+          <Route path={`/${reponame}/`} element={<Home />}/>
           <Route path={`/${reponame}/cars`} element={<Cars />}/>
           <Route path={`/${reponame}/street`} element={<Street />}/>
+          <Route path={`/${reponame}/bridges`} element={<Bridges />}/>
+          <Route path={`/${reponame}/buildings`} element={<Buildings />}/>
+          <Route path={`/${reponame}/skylines`} element={<Skylines />}/>
+          <Route path={`/${reponame}/bio`} element={<Bio />}/>
         </Routes>
       </div>
     </BrowserRouter>
