dd1e0d291614fe1bf599f578ba86a37eb851cabf
Graham Klingler <grahamklingler@gmail.com>
2023-10-01 20:18:00 -0700

    DROPDOWN WORKSgit add .git add .

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    document.addEventListener("mouseover", (e) => {
+        const isDropdownButton = e.target.matches("[data-dropdown-button]");
+        const isDropdown = e.target.closest("[data-dropdown]");
+      
+        if (!isDropdownButton && !isDropdown) {
+          // Close all dropdowns when mouse leaves the common ancestor
+          const dropdowns = document.querySelectorAll("[data-dropdown].active");
+          dropdowns.forEach((dropdown) => {
+            dropdown.classList.remove("active");
+          });
+        } else if (isDropdownButton && isDropdown) {
+          // Open the dropdown when the button is hovered over
+          isDropdown.classList.add("active");
+        }
+      });
+
     return (
         <div className="navbar">    
             <home className="navhome">
@@ -10,9 +26,9 @@ function Navbar() {
                 <li>
                     <Link to="/Portfolio-Project/bio" style={{ paddingRight: "40px" }}>About</Link>
                 </li>
-                <li className="dropdown" style={{ paddingLeft: "10px" }}>
-                    <button className="droplink" >Collections</button>
-                    <div className="dropdown-menu">
+                <li className="dropdown" style={{ paddingLeft: "10px" } } data-dropdown>
+                    <button className="droplink" data-dropdown-button>Collections</button>
+                    <div className="dropdown-menu" data-dropdown-menu>
                         <li>
                             <Link style={{ color: "black" }} to="/Portfolio-Project/bridges">Bridges</Link>
                         </li>
