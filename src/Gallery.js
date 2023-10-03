d54f82da407cd5b79491346c20fcec4ed49d182f
Graham Klingler <grahamklingler@gmail.com>
2023-10-03 16:57:47 -0700

    Add fullscreen toggle on click for img gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
+import CloseIcon from "@mui/icons-material/Close";
 
 function Gallery(props) {
-    return(
-        <div className='gallery'>
+    const [model, setModel] = useState(false);
+    const [tempImgSrc, setTempImgSrc] = useState('');
+    const getImg = (imgSrc) => {
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    } 
 
+    return(
+        <>
+        <div className={model? "model open" : "model"}>
+            <img src={tempImgSrc} alt="fullscreen"/>
+            <CloseIcon onClick={() => setModel(false)}/>
+        </div>
+        <div className='gallery'>
             {props.images.map((img, index) => {
                 return (
-                    <div className='img' key={index}>
+                    <div className='img' key={index} onClick={() => getImg(img)}>
                         <img key={index} src={img} loading="lazy" style={{width: "100%"}} alt={`${props.pathname}-${index}`}/>
                     </div>
                 )
             })}
         </div>
+        </>
     );
 }
 
